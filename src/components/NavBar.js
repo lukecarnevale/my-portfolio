a6053db23753a4a1918da2e8b8c8bd5403741b84
lukecarnevale <lukecarnevale19@gmail.com>
2021-02-05 10:09:32 -0500

    blog post cards connected to individual posts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,12 @@ import { SocialIcon } from 'react-social-icons'
 
 export default function NavBar () {
   return (
-    <header className='bg-blue-600'>
+    <header className='bg-green-900 bg-opacity-75'>
       <div className='container mx-auto flex justify-between'>
         <nav className='flex'>
           <NavLink
             to='/' exact
-            activeClassName='text-white'
+            activeClassName='text-blue-300'
             className='inline-flex items-center py-6 py-3 mr-4 text-blue-100 hover:text-green-200 text-4xl font-bold cursive tracking-widest'
           >
             Home
@@ -17,21 +17,21 @@ export default function NavBar () {
           <NavLink
             to='/post'
             activeClassName='text-blue-100 bg-blue-700'
-            className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-green-200'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-gray-200 hover:text-green-200'
           >
             Blog Post
           </NavLink>
           <NavLink
             to='project'
             activeClassName='text-blue-100 bg-blue-700'
-            className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-green-200'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-gray-200 hover:text-green-200'
           >
             Projects
           </NavLink>
           <NavLink
             to='about'
             activeClassName='text-blue-100 bg-blue-700'
-            className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-green-200'
+            className='inline-flex items-center py-3 px-3 my-6 rounded text-gray-200 hover:text-green-200'
           >
              About Me!
           </NavLink>
