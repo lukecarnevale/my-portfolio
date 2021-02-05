af8e8af8f0eb261fe3fc25deb982518c186b9a50
lukecarnevale <lukecarnevale19@gmail.com>
2021-02-04 22:24:10 -0500

    blog post section complete and synced to Sanity

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,21 +23,27 @@ export default function Post () {
   }, [])
 
   return (
-    <main className='bg-green-100'>
-      <section>
-        <h1> Blog Posts </h1>
-        <h2> Welcome to my page of blog posts </h2>
-        <div>
-          <article>
-            <Link>
-              <span>
-                <img />
-                <span>
-                  <h3 />
+    <main className='bg-green-100 min-h-screen p-12'>
+      <section className='container mx-auto'>
+        <h1 className='text-5xl flex justify-center cursive'> Blog Posts </h1>
+        <h2 className='text-lg text-gray-600 flex justify-center mb-12'> Welcome to my page of blog posts </h2>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          {postData && postData.map((post, index) => (
+            <article key={index}>
+              <Link to={'/post/' + post.slug.current} key={post.slug.current}>
+                <span className='block h-64 relative rounded shadow leading-snug background-white border-l-8 border-green-400' key={index}>
+                  <img
+                    src={post.mainImage.asset.url}
+                    alt={post.mainImage.alt}
+                    className='w-full h-full rounded-r object-cover absolute'
+                  />
+                  <span className='block relative h-full flex justify-end items-end pr-4 pb-4'>
+                    <h3 className='text-gray-100 text-lg font-bold px-3 py-4 bg-green-700 bg-opacity-75 rounded hover:text-blue-300'>{post.title}</h3>
+                  </span>
                 </span>
-              </span>
-            </Link>
-          </article>
+              </Link>
+            </article>
+          ))}
         </div>
       </section>
     </main>
